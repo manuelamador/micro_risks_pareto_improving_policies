363a89b35da803bab5c731f05c7ebd84e4f4e5d3
Manuel Amador <amador.manuel@gmail.com>
2021-08-08 15:26:31 +0000

    comparative statics

--- scripts/elasticities.jl
+++ scripts/elasticities.jl
@@ -3,7 +3,7 @@
 
 import Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
-Pkg.instantiate()
+# Pkg.instantiate()
 
 _FIGURES_DIR = joinpath(@__DIR__, "..", "output", "figures")
 
@@ -73,11 +73,15 @@ for β in β_vals[2:end]
     push!(pars, (ies = ies_vals[1], crra = crra_vals[1], β = β))
 end
 e_vals = creating_economies(pars);
-# -
 
+# +
 # Internal parameters for computations 
 _TOLS = (value_function = 1e-10, distribution = 1e-13) # (value_function = 1e-10, distribution = 1e-13)
 _ITERS = 100 # 100
+
+# Transitions lengths
+_T = 100  # period of adjustment
+_H = 50   # stationary part
 
 
 # +
@@ -89,78 +93,70 @@ function clean_transition_val(val)
     return (ies = ies, crra = crra, β = β, y0 = y0, e = e, path_r = path_r, path_tr = path_tr)
 end 
 
-function solve_models(e_vals; tols = _TOLS, iters = _ITERS)
+
+function solve_one_sim(e, i; T = _T,  H = _H, tols = _TOLS, iters = _ITERS)
+    println("Simulation # $i started")
+
+    # Solve laissez-faire economy
+    laissez_faire = let r_range = (-0.04, -0.00)
+        solve_laissez_faire(e; r_range = r_range, tol =  tols, verbose = false)
+    end
+
+    println("Finished laissez-faire for simulation # $i")
+
+    # We target a debt of 60% percent of output
+    b_target = laissez_faire.y * 0.60
+    k_target = laissez_faire.k
+
+    # Solve final SS
+    final_eq = solve_new_stationary_equilibrium_given_k_b(
+        laissez_faire, k_target, b_target;
+        r_range = (laissez_faire.r - 0.01, laissez_faire.r + 0.01), tol = tols, verbose = false
+    )
+    println("Finished new eqm for simulation # $i")
+
+    # Smooth debt policy
+    ρB = 0.9
+    b_list = Array{Float64,1}(undef, T + H)
+    b_list[1] = 0.0
+    b_list[2] = laissez_faire.y * 0.05
+    b_list[T:end] .= b_target
+    for j in 3:T-1
+        b_list[j] = b_list[2] * ρB^(j-2) + (1 - ρB^(j-2)) * b_target
+    end
+
+    # k remains constant
+    k_list = [laissez_faire.k for _ in b_list]
+
+    # Computes the transition
+    transition = solve_transition(
+        laissez_faire,
+        final_eq,
+        k_list,
+        b_list;
+        init_r_path = nothing,
+        iterations = iters,
+        show_trace = false)
+    
+    println("Finished transition for simulation # $i, residual = $(maximum(abs.(transition.residuals)))")    
+
+    return (e_vals[i]..., laissez_faire = laissez_faire, transition = transition)
+end
+
+
+function solve_all_sims(e_vals; tols = _TOLS, iters = _ITERS)
     #Iterate over preferences
     transitions = Array{Any}(undef, length(e_vals))
 
-    # ## Transitions
-    T = 100  # period of adjustment
-    H = 50   # stationary part
-
-    @floops for i in 1:length(e_vals)
-        
-        println("Simulation # $i / $(length(e_vals)) started")
-        
-        e = e_vals[i].e
-
-        # Solve laissez-faire economy
-        laissez_faire = let r_range = (-0.04, -0.00)
-            solve_laissez_faire(e;
-                r_range = r_range,
-                tol =  _TOLS, 
-                verbose = false
-            )
-        end
-        
-        println("Finished laissez-faire for simulation # $i")
-        
-        # We target a debt of 60% percent of output
-        b_target = laissez_faire.y * 0.60
-        k_target = laissez_faire.k
-
-        # Solve final SS
-        final_eq = solve_new_stationary_equilibrium_given_k_b(
-            laissez_faire,
-            k_target,
-            b_target;
-            r_range = (laissez_faire.r - 0.01, laissez_faire.r + 0.01),
-            tol = _TOLS, 
-            verbose = false
-        )
-        println("Finished new eqm for simulation # $i")
-
-        # Smooth debt policy
-        ρB = 0.9
-        b_list = Array{Float64,1}(undef, T + H)
-        b_list[1] = 0.0
-        b_list[2] = laissez_faire.y * 0.05
-        b_list[T:end] .= b_target
-        for j in 3:T-1
-            b_list[j] = b_list[2] * ρB^(j-2) + (1 - ρB^(j-2)) * b_target
-        end
-
-        # k remains constant
-        k_list = [laissez_faire.k for _ in b_list];
-
-        # Computes the transition
-        transition = solve_transition(
-            laissez_faire,
-            final_eq,
-            k_list,
-            b_list;
-            init_r_path = nothing,
-            iterations = _ITERS,
-            show_trace = false);
-        println("Finished transition for simulation # $i, residual = $(maximum(abs.(transition.residuals)))")
-
-        transitions[i] = (e_vals[i]..., laissez_faire = laissez_faire, transition = transition)
+    for i in eachindex(e_vals)
+        transitions[i] = solve_one_sim(e_vals[i].e, i, tols = tols, iters = iters) 
     end
     
     return [clean_transition_val(x) for x in transitions]
 end 
 # -
 
-@time out = solve_models(e_vals);
+@time out = solve_all_sims(e_vals);
 
 # ## Plots 
 
@@ -171,43 +167,49 @@ end;
 
 # ### IES
 
-f1 = plot(benchmark.path_r,  linecolor=:blue, lw = 2, size = (1000/3, 500/2))
-plot!(f1, [y.path_r for y in out if y.ies==0.5 && y.crra==5.5 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 2)
-plot!(f1, [y.path_r for y in out if y.ies==1.5 && y.crra==5.5 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 2)
+f1 = plot(100 .* benchmark.path_r,  linecolor=:blue, lw = 1.5, size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f1, 0, 100)
+plot!(f1, [100.0 * y.path_r for y in out if y.ies==0.5 && y.crra==5.5 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f1, [100.0 * y.path_r for y in out if y.ies==1.5 && y.crra==5.5 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f1, joinpath(_FIGURES_DIR, "IES_interest.pdf"))
 
-f2 = plot(benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 2,  size = (1000/3, 500/2))
-plot!(f2, [y.path_tr./y.y0 for y in out if y.ies==0.5 && y.crra==5.5 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 1)
-plot!(f2, [y.path_tr./y.y0 for y in out if y.ies==1.5 && y.crra==5.5 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 2)
+f2 = plot(100 * benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 1.5,  size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f2, 0, 100)
+plot!(f2, [100 * y.path_tr / y.y0 for y in out if y.ies==0.5 && y.crra==5.5 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f2, [100 * y.path_tr / y.y0 for y in out if y.ies==1.5 && y.crra==5.5 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f2, joinpath(_FIGURES_DIR, "IES_transfers.pdf"))
 
 # ### Discount
 
-f3 = plot(benchmark.path_r,  linecolor=:blue, lw = 2, size = (1000/3, 500/2))
-plot!(f3, [y.path_r for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.97], linecolor=:black, linestyle=:dash, lw = 2)
-plot!(f3, [y.path_r for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.98], linecolor=:red, linestyle=:dot, lw = 2)
+f3 = plot(100 .* benchmark.path_r,  linecolor=:blue, lw = 1.5, size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f3, 0, 100)
+plot!(f3, [100 * y.path_r for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.97], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f3, [100 * y.path_r for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.98], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f3, joinpath(_FIGURES_DIR, "Beta_interest.pdf"))
 
-f4 = plot(benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 2, size = (1000/3, 500/2))
-plot!(f4, [y.path_tr./y.y0 for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.97], linecolor=:black, linestyle=:dash, lw = 2)
-plot!(f4, [y.path_tr./y.y0 for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.98], linecolor=:red, linestyle=:dot, lw = 2)
+f4 = plot(100 .* benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 1.5, size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f4, 0, 100)
+plot!(f4, [100 * y.path_tr / y.y0 for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.97], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f4, [100 * y.path_tr / y.y0 for y in out if y.ies==1.0 && y.crra==5.5 && y.β==0.98], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f4, joinpath(_FIGURES_DIR, "Beta_transfers.pdf"))
 
 # ### CRRA
 
-f5 = plot(benchmark.path_r,  linecolor=:blue, lw = 2, size = (1000/3, 500/2))
-plot!(f5, [y.path_r for y in out if y.ies==1.0 && y.crra==2.0 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 2)
-plot!(f5, [y.path_r for y in out if y.ies==1.0 && y.crra==10.0 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 2)
+f5 = plot(100 .* benchmark.path_r,  linecolor=:blue, lw = 1.5, size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f5, 0, 100)
+plot!(f5, [100 * y.path_r for y in out if y.ies==1.0 && y.crra==2.0 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f5, [100 * y.path_r for y in out if y.ies==1.0 && y.crra==10.0 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f5, joinpath(_FIGURES_DIR, "CRRA_interest.pdf"))
 
-f6 = plot(benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 2, size = (1000/3, 500/2))
-plot!(f6, [y.path_tr./y.y0 for y in out if y.ies==1.0 && y.crra==2.0 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 2)
-plot!(f6, [y.path_tr./y.y0 for y in out if y.ies==1.0 && y.crra==10.0 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 2)
+f6 = plot(100 .* benchmark.path_tr./benchmark.y0,  linecolor=:blue, lw = 1.5, size = (1000/3, 500/2), ylabel = L"\%")
+xlims!(f6, 0, 100)
+plot!(f6, [100 * y.path_tr/y.y0 for y in out if y.ies==1.0 && y.crra==2.0 && y.β==0.993], linecolor=:black, linestyle=:dash, lw = 1.5)
+plot!(f6, [100 * y.path_tr/y.y0 for y in out if y.ies==1.0 && y.crra==10.0 && y.β==0.993], linecolor=:red, linestyle=:dot, lw = 1.5)
 
 savefig(f6, joinpath(_FIGURES_DIR, "CRRA_transfers.pdf"))
 
